6c49a2fe51b50b74d40bea8ce8a70f7ed78076f9
Tenchigreed <72823178+Tenchigreed@users.noreply.github.com>
2021-06-30 09:06:34 +0200

    Update auth.py

--- auth.py
+++ auth.py
@@ -8,10 +8,10 @@ Created on Mon Mar 29 10:58:47 2021
 import tweepy
 
 def auth():
-    CONSUMER_KEY = '5Ly7e8KM4BHn5qQQtTE4Cl7FY'
-    CONSUMER_SECRET = 'F9PYqqHlm6NPrco7fANXjs5X1iKbKj841wfYDxzJFuqC1W7fyQ'
-    ACCESS_KEY ='1376542724082561032-Az6l5bAO1qkhVf1CfGtH733n3amtCz'
-    ACCESS_SECRET = 'rn4qvoQUpOu4vaBuhPOGbiAshFTTL2Jbom8tkh1NDEby7'
+    CONSUMER_KEY = ''
+    CONSUMER_SECRET = ''
+    ACCESS_KEY =''
+    ACCESS_SECRET = ''
     
     auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
     auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
